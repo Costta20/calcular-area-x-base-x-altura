7c0a79b2d7131ca5e5646132caf19c7422aee303
samuel.jcosta2 <samuel.jcosta2@CAS521466A106D.senacsp.edu.br>
2023-02-17 21:45:23 -0300

    finish

---
+++ src/main/java/com/mycompany/aula02/Aula02.java
@@ -0,0 +1,18 @@
+    /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aula02;
+
+/**
+ *
+ * @author samuel.jcosta2
+ */
+public class Aula02 {
+
+    public static void main(String[] args) {
+     Retangulo re = new Retangulo();
+     re.calcularArea();
+     
+    }
+}
